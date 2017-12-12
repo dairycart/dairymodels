4000ccfa603d36dde296f5273871f390465f3997
Jeffrey D <verygoodsoftwarenotvirus@users.noreply.github.com>
2017-12-12 09:25:53 -0600

    Modify UserCreation/UpdateInput structs

--- v1/users.go
+++ v1/users.go
@@ -2,38 +2,39 @@ package models
 
 // User represents a Dairycart user
 type User struct {
-	ID                    uint64     `json:"id"`                       // id
-	FirstName             string     `json:"first_name"`               // first_name
-	LastName              string     `json:"last_name"`                // last_name
-	Username              string     `json:"username"`                 // username
 	Email                 string     `json:"email"`                    // email
-	Password              string     `json:"password"`                 // password
-	Salt                  []byte     `json:"salt"`                     // salt
-	IsAdmin               bool       `json:"is_admin"`                 // is_admin
-	PasswordLastChangedOn *Dairytime `json:"password_last_changed_on"` // password_last_changed_on
 	CreatedOn             *Dairytime `json:"created_on"`               // created_on
-	UpdatedOn             *Dairytime `json:"updated_on"`               // updated_on
 	ArchivedOn            *Dairytime `json:"archived_on"`              // archived_on
+	FirstName             string     `json:"first_name"`               // first_name
+	UpdatedOn             *Dairytime `json:"updated_on"`               // updated_on
+	ID                    uint64     `json:"id"`                       // id
+	Username              string     `json:"username"`                 // username
+	PasswordLastChangedOn *Dairytime `json:"password_last_changed_on"` // password_last_changed_on
+	Salt                  []byte     `json:"salt"`                     // salt
+	LastName              string     `json:"last_name"`                // last_name
+	IsAdmin               bool       `json:"is_admin"`                 // is_admin
+	Password              string     `json:"password"`                 // password
 }
 
 // UserCreationInput is a struct to use for creating product options
 type UserCreationInput struct {
-	FirstName string `json:"first_name,omitempty"` // first_name
-	LastName  string `json:"last_name,omitempty"`  // last_name
-	Username  string `json:"username,omitempty"`   // username
 	Email     string `json:"email,omitempty"`      // email
-	Password  string `json:"password,omitempty"`   // password
+	FirstName string `json:"first_name,omitempty"` // first_name
+	Username  string `json:"username,omitempty"`   // username
+	LastName  string `json:"last_name,omitempty"`  // last_name
 	IsAdmin   bool   `json:"is_admin,omitempty"`   // is_admin
+	Password  string `json:"password,omitempty"`   // password
 }
 
 // UserUpdateInput is a struct to use for updating Users
 type UserUpdateInput struct {
-	FirstName string `json:"first_name,omitempty"` // first_name
-	LastName  string `json:"last_name,omitempty"`  // last_name
-	Username  string `json:"username,omitempty"`   // username
-	Email     string `json:"email,omitempty"`      // email
-	Password  string `json:"password,omitempty"`   // password
-	IsAdmin   bool   `json:"is_admin,omitempty"`   // is_admin
+	Email           string `json:"email,omitempty"`            // email
+	FirstName       string `json:"first_name,omitempty"`       // first_name
+	Username        string `json:"username,omitempty"`         // username
+	LastName        string `json:"last_name,omitempty"`        // last_name
+	IsAdmin         bool   `json:"is_admin,omitempty"`         // is_admin
+	NewPassword     string `json:"new_password,omitempty"`     // password
+	CurrentPassword string `json:"current_password,omitempty"` // password
 }
 
 type UserListResponse struct {
