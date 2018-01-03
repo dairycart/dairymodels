54306456abef06ee9f6eb35eceae1a8bffac6c62
Jeffrey D <verygoodsoftwarenotvirus@users.noreply.github.com>
2018-01-03 05:52:59 -0600

    Create product image bridges

--- v1/product_images.go
+++ v1/product_images.go
@@ -6,15 +6,15 @@ import (
 
 // ProductImage represents a Dairycart product image
 type ProductImage struct {
-	ID           uint64     `json:"id"`            // id
-	ProductID    uint64     `json:"product_id"`    // product_id
-	ThumbnailURL string     `json:"thumbnail_url"` // thumbnail_url
-	MainURL      string     `json:"main_url"`      // main_url
-	OriginalURL  string     `json:"original_url"`  // original_url
-	SourceURL    string     `json:"source_url"`    // source_url
-	CreatedOn    time.Time  `json:"created_on"`    // created_on
-	UpdatedOn    *Dairytime `json:"updated_on"`    // updated_on
-	ArchivedOn   *Dairytime `json:"archived_on"`   // archived_on
+	ID            uint64     `json:"id"`              // id
+	ProductRootID uint64     `json:"product_root_id"` // product_root_id
+	ThumbnailURL  string     `json:"thumbnail_url"`   // thumbnail_url
+	MainURL       string     `json:"main_url"`        // main_url
+	OriginalURL   string     `json:"original_url"`    // original_url
+	SourceURL     string     `json:"source_url"`      // source_url
+	CreatedOn     time.Time  `json:"created_on"`      // created_on
+	UpdatedOn     *Dairytime `json:"updated_on"`      // updated_on
+	ArchivedOn    *Dairytime `json:"archived_on"`     // archived_on
 }
 
 // ProductImageCreationInput is a struct to use for creating ProductImages
@@ -26,10 +26,10 @@ type ProductImageCreationInput struct {
 
 // ProductImageUpdateInput is a struct to use for updating ProductImages
 type ProductImageUpdateInput struct {
-	ProductID    uint64 `json:"product_id,omitempty"`    // product_id
-	ThumbnailURL string `json:"thumbnail_url,omitempty"` // thumbnail_url
-	MainURL      string `json:"main_url,omitempty"`      // main_url
-	OriginalURL  string `json:"original_url,omitempty"`  // original_url
+	ProductRootID uint64 `json:"product_root_id,omitempty"` // product_root_id
+	ThumbnailURL  string `json:"thumbnail_url,omitempty"`   // thumbnail_url
+	MainURL       string `json:"main_url,omitempty"`        // main_url
+	OriginalURL   string `json:"original_url,omitempty"`    // original_url
 }
 
 type ProductImageListResponse struct {
